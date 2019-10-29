b5d5799e1661add2a9f3a54e9af55af570595953
KANG <KANG@DESKTOP-7Q5N3AD>
2019-10-29 09:29:20 +0900

    까페 메뉴 생성

---
+++ SFCafe/src/com/kang/sfcafe/Main.java
@@ -0,0 +1,7 @@
+package com.kang.sfcafe;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ SFCafe/src/com/kang/sfcafe/MenuItem.java
@@ -0,0 +1,11 @@
+package com.kang.sfcafe;
+
+public class MenuItem {
+	private String menuName;
+	private int menuPrice;
+	
+	public MenuItem(String menuName, int menuPrice) {
+		this.menuName = menuName;
+		this.menuPrice = menuPrice;
+	}
+}
