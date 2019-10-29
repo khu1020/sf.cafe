24011ac326072c53ceab66a6c0be2fade7a75b22
KANG <KANG@DESKTOP-7Q5N3AD>
2019-10-29 10:51:57 +0900

    메뉴판 생성, 메뉴판 값넣기

--- SFCafe/src/com/kang/sfcafe/Main.java
+++ SFCafe/src/com/kang/sfcafe/Main.java
@@ -2,6 +2,8 @@ package com.kang.sfcafe;
 
 public class Main {
 	public static void main(String[] args) {
+		MenuTable mt = new MenuTable();
 		
+		mt.showMenus();
 	}
 }
--- SFCafe/src/com/kang/sfcafe/MenuTable.java
+++ SFCafe/src/com/kang/sfcafe/MenuTable.java
@@ -3,12 +3,29 @@ package com.kang.sfcafe;
 import java.util.*;
 
 public class MenuTable {
-	private List<MenuItem> menuTable = new ArrayList();
+	private List<MenuItem> menus = new ArrayList();
 	
 	public MenuTable () {
-		menuTable.add(new MenuItem("아메리카노", 1500));
-		menuTable.add(new MenuItem("카푸치노", 2000));
-		menuTable.add(new MenuItem("카라멜 마끼야또", 2500));
-		menuTable.add(new MenuItem("에스프레소", 3000));
+		menus.add(new MenuItem("아메리카노", 1500));
+		menus.add(new MenuItem("카푸치노", 2000));
+		menus.add(new MenuItem("카라멜 마키아또", 2500));
+		menus.add(new MenuItem("에스프레소", 3000));
+	}
+	
+	public void showMenus() {
+		System.out.printf("------- MENU -------\n");
+		for(int i = 0; i < menus.size(); i++) {
+			MenuItem m = menus.get(i);
+			System.out.printf("%d. %s - (%,d원) \n", (i+1), m.getMenuName(), m.getMenuPrice());
+		}
+		System.out.printf("--------------------\n");
+	}
+	
+	public MenuItem choiceMenu(int idx) {
+		if((idx-1) < 0 || (idx-1) >= menus.size()) {
+			System.out.print("올바르지 않는 입력입니다.\n");
+			return null;
+		}
+		return menus.get(idx - 1);
 	}
 }
